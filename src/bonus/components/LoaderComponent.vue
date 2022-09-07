aa551948b079832609d8f09b143328fbbc7d99b4
giusscos <giu.cosenza08@gmail.com>
2022-09-07 17:11:08 +0200

    Aggiunta stile e animazione loader

--- src/bonus/components/LoaderComponent.vue
+++ src/bonus/components/LoaderComponent.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="loader">
-        <h1>
-            Carico...
-        </h1>
+    <div class="loader_wrapper">
+        <div class="loader">
+            <span class="square_center"> Loading... </span>
+            <span class="square top"></span>
+            <span class="square bottom"></span>
+        </div>
     </div>
 </template>
 <script>
@@ -13,14 +15,51 @@ export default {
 <style lang="scss" scoped>
 @import '../../style/index.scss';
 
-.loader{
+.loader_wrapper{
     position: absolute;
     top:50%;
     left: 50%;
     transform: translate(-50%);
 
-    h1{
-        color: $ds-accent_ligth;
+    .loader{
+        position: relative;
+        width: 100px;
+        aspect-ratio: 3/2;
+
+        .square{
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform: translate(-50%, -50%);
+            
+            width: 2rem;
+            height: 2rem;
+            border-radius: 50%;
+            aspect-ratio: 1/1;
+            background-color: $ds-accent_ligth;
+
+            &.top{
+                top: 0;
+                left: 100%;
+                animation: loaderDotTop infinite 2000ms linear;
+            }
+            &.bottom{
+                top: 100%;
+                left: 0;
+                animation: loaderDotBot infinite 2000ms linear;
+            }
+        }
+        .square_center{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            
+            color: $ds-bg;
+            padding: 2rem;
+            font-weight: bold;
+            border-radius: 0.5rem;
+            background-color: $ds-accent_ligth;
+        }
     }
 }
 </style>
