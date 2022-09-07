6cb1a304031475ec3bb8aea06a6f74935e6ad073
giusscos <giu.cosenza08@gmail.com>
2022-09-07 16:20:20 +0200

    Aggiunta props

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="card_img-wrapper">
+            <img :src="card.poster" :alt="card.title" />
+        </div>
+        <h3 class="card_title">
+            {{ card.title }}
+        </h3>
+        <p class="card_desc">
+            {{ card.author }} <br>
+            {{ card.year }}
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CardComponent',
+    props: {
+        card: Object,
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '../style/index.scss';
+
+.card_title {
+    padding: 1rem 0;
+    font-weight: bold;
+}
+
+.card_desc {
+    color: $ds-accent_dark;
+}
+</style>    
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -3,40 +3,33 @@
         <div class="cards_wrapper container">
             <ul class="cards_list">
                 <li class="list_item card" v-for="(album, i) in albums" :key="`${i}alb`">
-                    <div class="card_img-wrapper">
-                        <img :src="album.poster" :alt="album.title" />
-                    </div>
-                    <h3 class="card_title">
-                        {{ album.title }}
-                    </h3>
-                    <p class="card_desc">
-                        {{ album.author }} <br>
-                        {{ album.year }}
-                    </p>
+                    <CardComponent :card="album" />
                 </li>
             </ul>
         </div>
     </main>
 </template>
+
 <script>
 import axios from 'axios';
+import CardComponent from './CardComponent.vue';
 
 export default {
-
-    name: 'MainComponent',
+    name: "MainComponent",
     data() {
         return {
             albums: []
-        }
+        };
     },
     mounted() {
         axios
-            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .get("https://flynn.boolean.careers/exercises/api/array/music")
             .then((res) => {
                 // console.log(res.data.response)
-                this.albums = res.data.response
-            })
-    }
+                this.albums = res.data.response;
+            });
+    },
+    components: { CardComponent }
 }
 </script>
 <style lang="scss" scoped>
@@ -63,15 +56,6 @@ main {
             padding: 1rem 1rem 2rem;
             background-color: $ds-accent;
             flex-basis: calc((100% / 5) - 1rem);
-
-            .card_title {
-                padding: 1rem 0;
-                font-weight: bold;
-            }
-
-            .card_desc{
-                color: $ds-accent_dark;
-            }
         }
     }
 }
