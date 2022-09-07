dc14a62a943812af3cab40b97113ffa710e78039
giusscos <giu.cosenza08@gmail.com>
2022-09-07 15:26:29 +0200

    Aggiunta Header e Main

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <header>    
-        <h1>Hello World!</h1>
+        <div class="logo_wrapper">
+            <img height="50px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpnggrid.com%2Fwp-content%2Fuploads%2F2021%2F05%2FSpotify-PNG-Logo-1536x1534.png&f=1&nofb=1" alt="logo-spotify"/>
+        </div>
     </header>   
 </template>
 <script>
@@ -9,5 +11,14 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+    @import '../style/index.scss';
 
+    header{
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        padding: 0.5rem 1rem;
+        background-color: $ds-accent;
+    }
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <main>
+        
+    </main>
+</template>
+<script>
+export default {
+    name: 'MainComponent',
+}
+</script>
+<style lang="scss" scoped>
+@import '../style/index.scss';
+
+main{
+    height: 100vh;
+    background-color: $ds-bg;
+}
+</style>
